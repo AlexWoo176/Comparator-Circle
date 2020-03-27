5f767e324a4f831a7d11e6e79a05659adb7a196d
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-28 00:18:00 +0700

    Complete

---
+++ src/comparator/circle/Circle.java
@@ -0,0 +1,44 @@
+package comparator.circle;
+
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, Boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.pow(radius,2) * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius = "
+                + getRadius()
+                + " has area = "
+                + getArea()
+                + " and perimeter = "
+                + getPerimeter()
+                + " which is a subclass of " + super.toString();
+    }
+}
